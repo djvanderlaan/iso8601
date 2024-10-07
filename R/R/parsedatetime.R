3247a6ff2eee8b122f6e8b043a110d8335cdadb6
Jan van der Laan <jan@ntteloos.nl>
2024-10-07 16:22:58 +0200

    R parsedatetime

--- R/R/parsedatetime.R
+++ R/R/parsedatetime.R
@@ -10,5 +10,25 @@ parsedatetime <- function(x) {
   res$gmtoff <- ifelse(res$zone == "Z", 0L, NA_integer_) 
   res <- res[c("sec", "min", "hour", "mday", "mon", "year", "wday", "yday", 
     "isdst", "zone", "gmtoff")]
+  local <- all(res$zone == "")
+  gmt <- all(res$zone != "")
+  tzone <- if (local) "" else if (gmt) "GMT" else ""
+  if (gmt) res$zone <- "GMT"
   structure(res, tzone = "", class = c("POSIXlt", "POSIXT"))
 }
+
+
+res <- parsedatetime(c("2024-01-01T12:30", "2024-01-01T12:30"))
+res
+onclass(res)
+
+#
+#
+#local <- all(res$zone == "")
+#local
+#
+#gmt <- all(res$zone != "")
+#gmt
+#
+#unique(res$zone)
+#
