97b5d10739ce33d13d8677351c922a1b947716d2
Jan van der Laan <jan@ntteloos.nl>
2024-10-11 10:52:56 +0200

    parsedatetime

--- R/R/parsedatetime.R
+++ R/R/parsedatetime.R
@@ -1,5 +1,5 @@
 
-
+#' @export
 parsedatetime <- function(x) {
   res <- rcpp_parse_datetime(x)
   n <- length(x)
@@ -8,27 +8,15 @@ parsedatetime <- function(x) {
   res$yday <- rep(NA_integer_, n)
   res$isdst <- rep(0L, n)
   res$gmtoff <- ifelse(res$zone == "Z", 0L, NA_integer_) 
-  res <- res[c("sec", "min", "hour", "mday", "mon", "year", "wday", "yday", 
-    "isdst", "zone", "gmtoff")]
+  res <- res[c("sec", "min", "hour", "mday", "mon", "year", "wday", 
+    "yday", "isdst", "zone", "gmtoff")]
   local <- all(res$zone == "")
   gmt <- all(res$zone != "")
-  tzone <- if (local) "" else if (gmt) "GMT" else ""
-  if (gmt) res$zone <- "GMT"
-  structure(res, tzone = "", class = c("POSIXlt", "POSIXT"))
+  tzone <- if (local) "" else "GMT"
+  res$zone[res$zone != ""] <- "GMT"
+  if (!local && !gmt)
+    warning("Mix of local time zone and GMT. Assuming GMT is local time.")
+  structure(res, tzone = tzone, class = c("POSIXlt", "POSIXT"))
 }
 
 
-#res <- parsedatetime(c("2024-01-01T12:30", "2024-01-01T12:30"))
-#res
-#unclass(res)
-
-#
-#
-#local <- all(res$zone == "")
-#local
-#
-#gmt <- all(res$zone != "")
-#gmt
-#
-#unique(res$zone)
-#
